9205f96676392c90902bf93e183efde09a58b8ba
Satheesh Eppalapelli <40909746+satheesheppalapelli@users.noreply.github.com>
2019-04-11 14:48:31 +0530

    list comprehension

--- Dict_Comp.py
+++ Dict_Comp.py
@@ -1,3 +1,9 @@
+# dict_comprehension
 nums = [1,2,3,4,5,6,7,8]
 dict_comprehesion = {str(n): {"The corresponding key is: " + str(n)} for n in nums}
 print(dict_comprehesion)
+
+# list_Compreshion
+nums = 'human beings'
+list_comprehesion = [n for n in nums]
+print(list_comprehesion)
